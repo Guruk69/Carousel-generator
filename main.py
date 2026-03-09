fb173dd689414a7dae0ae155ad57e1f323cc3f07
unknown <gurukattishettar169@gmail.com>
2026-03-09 18:15:33 +0530

    resolved cors issue

--- main.py
+++ main.py
@@ -48,15 +48,16 @@ from PIL import Image
 from dotenv import load_dotenv
 load_dotenv()
 
-from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://carousel-frontend-rho.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
